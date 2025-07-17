b497b4d71a51eacfd5ecfea338d19434c37eeecf
GrahamPilk <graham.a.pilkington@Gmail.com>
2025-07-17 16:42:15 -0400

    added sections, some stuff to first page

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,34 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li> 
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <section className={styles.sectionWhite}>
+        <div className={styles.introContainer}>
+          <h1 className={styles.introTitle}>Hi, I am Graham Pilkington!</h1>
+          <div className={styles.ovalTab}>
+            <a href="https://github.com/GrahamPilk" target="_blank" rel="noopener noreferrer" className={styles.ovalLink}>GitHub</a>
+            <a href="/grahamresume.pdf" download className={styles.ovalLink}>Download Resume</a>
+          </div>
+          <p className={styles.introTopP}>Computer Science Student</p>
+          <p>I like to build!</p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to idk →
-        </a>
-      </footer>
+      </section>
+      <section className={styles.sectionBlue}>
+        <h1>About Me</h1>
+      </section>
+      <section className={styles.sectionWhite}>
+        <h1>Tech Arsenal</h1>
+      </section>
+      <section className={styles.sectionBlue}>
+        <h1>Experience</h1>
+      </section>
+      <section className={styles.sectionWhite}>
+        <h1>Projects</h1>
+      </section>
+      <section className={styles.sectionBlue}>
+        <h1>Contact Me</h1>
+      </section>
     </div>
   );
 }
